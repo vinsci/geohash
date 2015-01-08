66dc9911825eb9cfba6dd8c91c37927c9c65d6de
Leonard Norrgård <vinsci.sw@gmail.com>
2015-01-09 00:17:34 +0200

    Ensure test is done on source tree, not an installed version.

--- test/testgeohash.py
+++ test/testgeohash.py
@@ -1,3 +1,10 @@
+import sys
+
+# Test the source tree, not an installed version.
+sys.path.insert(0, '../Geohash')
+
+import geohash
+
 if __name__ == '__main__':
     import doctest
     print "Testing tests in README.rst..."
